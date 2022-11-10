9fd2d72b24743408ea1685bfdac301b6e976575a
hectorgtez <hectorgtez@gmail.com>
2022-11-09 21:00:04 -0700

    [ADD] Avance JS - Ciclos while

---
+++ 05 Javascript/cicloWhile.js
@@ -0,0 +1,15 @@
+/* Cliclo del 1 al 10 */
+var numero = 1;
+
+while(numero <= 10) {
+  document.write(numero + '<br>');
+  numero++;
+}
+
+/* Ciclo do while */
+var numero2 = 1;
+
+do {
+  document.write(numero2 + '<br>');
+  numero2++;
+} while(numero2 <= 10);
