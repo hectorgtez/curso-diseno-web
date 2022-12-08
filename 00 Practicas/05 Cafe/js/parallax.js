332e0009b2070db3229efc1472e02e3fa324dccf
hectorgtez <hectorgtez@gmail.com>
2022-12-07 17:59:45 -0700

    [ADD] Practica cafe terminada

--- 00 Practicas/05 Cafe/js/parallax.js
+++ 00 Practicas/05 Cafe/js/parallax.js
@@ -1,25 +1,19 @@
-$(document).ready(function(){
-	$(window).scroll(function(){
+$(document).ready(function() {
+
+	$(window).scroll(function() {
 		var windowWidth = $(window).width();
-		if (windowWidth > 800){
+		if(windowWidth >= 800) {
 			var scroll = $(window).scrollTop();
-
-			$('header .textos').css({
-				'transform': 'translate(0px,' + scroll / 2 + '%)'
-			});
-
-			$('.acerca-de article').css({
-				'transform': 'translate(0px, -' + scroll / 4 + '%)'
-			});
+			$("header .textos").css({ transform: "translate(0px, " + scroll / 2 + "%)" });
+			$(".acerca-de article").css({ transform: "translate(0px, -" + scroll / 4 + "%)" });
 		}
 	});
 
-	$(window).resize(function(){
+	$(window).resize(function() {
 		var windowWidth = $(window).width();
-		if (windowWidth < 800){
-			$('.acerca-de article').css({
-					'transform': 'translate(0px, 0px)'
-			});
+		if(windowWidth < 800) {
+			$(".acerca-de article").css({ transform: "translate(0px, 0px)" });
 		}
 	});
+
 });
